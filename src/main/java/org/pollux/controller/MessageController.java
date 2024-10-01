ce4ecc349e3cd3f8ebe5813116c92200c1581f26
Nikita <good@quadrk.space>
2024-10-01 16:16:21 +0200

    Log4j2 added

--- src/main/java/org/pollux/controller/MessageController.java
+++ src/main/java/org/pollux/controller/MessageController.java
@@ -1,19 +1,20 @@
 package org.pollux.controller;
 
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
-import java.io.FileWriter;
+
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.Objects;
 
-
+@Slf4j
 public class MessageController extends TelegramLongPollingBot {
 
     public MessageController(String botToken) throws IOException {
@@ -26,40 +27,20 @@ public class MessageController extends TelegramLongPollingBot {
         message.setText(text);
         execute(message);
     }
-    private void logUserMessage(String username, String message) throws TelegramApiException {
-        String filename = "data/logs/user_logs/" + username + "_logs.txt";
-        try (FileWriter writer = new FileWriter(filename, true)){
-            writer.append(message).append('\n').flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Override
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText();
-            String username = update.getMessage().getFrom().getUserName();
-            Date date = new Date();
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            String formattedDate = sdf.format(date);
-            if(username.isEmpty()) {
-                username = "user_" + update.getMessage().getFrom().getId();
-            }
             if(Objects.equals(message, "/start")) {
                 try {
                     sendMessage(update.getMessage().getChatId(), "Hello " + update.getMessage().getFrom().getFirstName() + "!");
-                    logUserMessage(username, message);
+                    log.info("Command received: {}", message);
                 } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
+                    log.error(e.getMessage());
                 }
             } else {
-                String formatted_text = "Received message: \"" + message + "\" username: @" + username + " date: " + formattedDate;
-                try {
-                    logUserMessage(username, formatted_text);
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
+                log.info("Message received: {}", message);
             }
 
         } else {
@@ -70,10 +51,6 @@ public class MessageController extends TelegramLongPollingBot {
             }
         }
 
-    }
-    @Override
-    public void onRegister() {
-        super.onRegister();
     }
 
     @Override
